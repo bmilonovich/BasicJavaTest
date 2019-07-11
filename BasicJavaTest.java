1abd8e0414516ada2b583023baf966cfd3518511
Brandon Milonovich <bmilonovich@ardsleyschools.org>
2019-07-10 18:57:45 -0700

    Added file

---
+++ BasicJavaTest.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class BasicJavaTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BasicJavaTest
+{
+
+
+    public static void main(String[] args)
+    {
+        // Run this program, add a similar line after.
+        System.out.println("Hello, world!");
+    }
+}
