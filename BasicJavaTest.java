a778bb89051f1b6e72eafd5593669111915d656b
Brandon Milonovich <bmilonovich@ardsleyschools.org>
2019-09-03 09:11:18 -0400

    One line of code added.

--- BasicJavaTest.java
+++ BasicJavaTest.java
@@ -14,5 +14,8 @@ public class BasicJavaTest
         // Run this program, add a similar line after.
         System.out.println("Hello, world!");
         System.out.println("Test number 2");
+        
+        // Added through BlueJ
+        System.out.println("This was added just now.");
     }
 }
